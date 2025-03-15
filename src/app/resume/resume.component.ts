97a8c1b7e96a7d24fa9e88e39750d6b247d80b20
illusionist <iamraviraj5@gmail.com>
2025-03-15 18:28:29 +0530

    final

--- src/app/resume/resume.component.ts
+++ src/app/resume/resume.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-resume',
   templateUrl: './resume.component.html',
   styleUrl: './resume.component.css'
 })
-export class ResumeComponent {
+export class ResumeComponent implements OnInit {
+
+
+  ngOnInit(): void {
+  }
+
+  downloadCV() {
+    const resumeLink = "https://drive.google.com/file/d/1q3l95YuDxW5VaHv28kqJNQvL1_SdMwkp/view";
+    window.open(resumeLink, '_blank');
+  }
 
 }
